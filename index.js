50a59414b5bf8a86d53467c60b38b5f2fe01b8da
Landon <landoncramer1@gmail.com>
2023-07-13 09:19:06 -0700

    event acting

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+
+function moveDodgerLeft(event) {
+    console.log(event);
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+
+    if(left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+function moveDodgerRight(event) {
+    console.log(event);
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+    
+    if(left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener('keydown', function(event) {
+    if(event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
