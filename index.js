83aa06a04870243bee6871ae208e31f2e4f7dca2
Landon <landoncramer1@gmail.com>
2023-09-15 06:08:15 -0700

    events lab2

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
-const dodger = document.getElementById("dodger");
+// const dodger = document.getElementById("dodger");
 
-dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.backgroundColor = "yellow";
 
 
-function moveDodgerLeft(event) {
-    console.log(event);
-    const leftNumber = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumber, 10);
+// function moveDodgerLeft(event) {
+//     console.log(event);
+//     const leftNumber = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumber, 10);
 
-    if(left > 0) {
-        dodger.style.left = `${left - 1}px`;
-    }
-}
-function moveDodgerRight(event) {
-    console.log(event);
-    const leftNumber = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumber, 10);
+//     if(left > 0) {
+//         dodger.style.left = `${left - 1}px`;
+//     }
+// }
+// function moveDodgerRight(event) {
+//     console.log(event);
+//     const rightNumber = dodger.style.left.replace("px", "");
+//     const right = parseInt(rightNumber, 10);
     
-    if(left < 360) {
-        dodger.style.left = `${left + 1}px`;
+//     if(right < 360) {
+//         dodger.style.left = `${right + 1}px`;
+//     }
+// }
+// document.addEventListener('keydown', function(event) {
+//     if(event.key === "ArrowLeft") {
+//         moveDodgerLeft();
+//     } else if (event.key === "ArrowRight") {
+//         moveDodgerRight();
+//     }
+// })
+
+
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor= "blue";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() { 
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+
+    if(right < 360) {
+        dodger.style.left = `${right + 1}px`;
     }
 }
+
 document.addEventListener('keydown', function(event) {
-    if(event.key === "ArrowLeft") {
-        moveDodgerLeft();
-    } else if (event.key === "ArrowRight") {
-        moveDodgerRight();
-    }
-})
+      if(event.key === "ArrowLeft") {
+          moveDodgerLeft();
+      } else if (event.key === "ArrowRight") {
+          moveDodgerRight();
+      }
+  })
+  
